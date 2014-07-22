5d4b09e641ee921ca1d0b5756975fd3718c4ef6c
Rafael J Torres <rafaelt88@gmail.com>
2014-07-22 08:53:36 -0430

    CRUD Role

---
+++ protected/views/controller/create.php
@@ -0,0 +1,15 @@
+<?php
+$this->breadcrumbs=array(
+	'Controllers'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List Controller', 'url'=>array('index')),
+	array('label'=>'Manage Controller', 'url'=>array('admin')),
+);
+?>
+
+<h1>Create Controller</h1>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
