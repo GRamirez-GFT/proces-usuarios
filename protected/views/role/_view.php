2fcbe5a4350df7506c868827ea5a94eb0b4ba36c
Rafael J Torres <rafaelt88@gmail.com>
2014-07-22 09:00:11 -0430

    Ficheros Role

---
+++ protected/views/role/_view.php
@@ -0,0 +1,19 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('company_id')); ?>:</b>
+	<?php echo CHtml::encode($data->company_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('product_id')); ?>:</b>
+	<?php echo CHtml::encode($data->product_id); ?>
+	<br />
+
+</div>
