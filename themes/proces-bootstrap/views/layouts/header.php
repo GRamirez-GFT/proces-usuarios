4c1d323e1f2b66c532c86a9488efbdcf91be62a7
gonzalezfx <gonzalez_fx@hotmail.com>
2015-04-20 15:38:49 -0500

    Se eliminan rutas minificadas en version de servidor

--- themes/proces-bootstrap/views/layouts/header.php
+++ themes/proces-bootstrap/views/layouts/header.php
@@ -10,13 +10,13 @@ Yii::app()->clientScript->registerLinkTag('shortcut icon', null, $this->assets .
     ->registerCssFile($this->assets . '/css/visualsearch.css')
     ->registerCssFile($this->assets . '/css/bootstrap.min.css')
     ->registerCssFile($this->assets . '/css/font-awesome.min.css')
-    ->registerCssFile($this->assets . '/css/select2' . (YII_DEBUG ? null : '.min') . '.css')
-    ->registerCssFile($this->assets . '/css/tipsy' . (YII_DEBUG ? null : '.min') . '.css')
-    ->registerCssFile($this->assets . '/css/style' . (1 ? null : '.min') . '.css')
+    ->registerCssFile($this->assets . '/css/select2.css')
+    ->registerCssFile($this->assets . '/css/tipsy.css')
+    ->registerCssFile($this->assets . '/css/style.css')
     ->registerCoreScript('jquery')
     ->registerScriptFile($this->assets . '/js/bootstrap.min.js', CClientScript::POS_END)
     ->registerScriptFile($this->assets . '/js/select2.min.js', CClientScript::POS_END)
     ->registerScriptFile($this->assets . '/js/jquery.tipsy.min.js', CClientScript::POS_END)
-    ->registerScriptFile($this->assets . '/js/functions' . (YII_DEBUG ? null : '.min') . '.js', CClientScript::POS_END);
+    ->registerScriptFile($this->assets . '/js/functions.js', CClientScript::POS_END);
 ?>
 </head>
